f49269402b41439eb22af9d68f7b86c2bf73b984
GregMac <gregorypotesgonzalez@gmail.com>
2024-07-08 14:16:31 +0200

    added ast_new_function, ast_new_unary, ast_new_binary

--- syntax_analysis.c
+++ syntax_analysis.c
@@ -18,3 +18,36 @@ ast_t *ast_new_variable(char *name, var_type_e type) {
   }
   return variable;
 }
+
+ast_t *ast_new_binary(ast_binary_e op, ast_t *left, ast_t *right) {
+    ast_t *node = (ast_t*)malloc(sizeof(ast_t));
+    if (node) {
+        node->type = AST_BINARY;
+        node->binary.op = op;
+        node->binary.left = left;
+        node->binary.right = right;
+    }
+    return node;
+}
+
+ast_t *ast_new_unary(ast_unary_e op, ast_t *operand) {
+    ast_t *node = (ast_t*)malloc(sizeof(ast_t));
+    if (node) {
+        node->type = AST_UNARY;
+        node->unary.op = op;
+        node->unary.operand = operand;
+    }
+    return node;
+}
+
+ast_t *ast_new_function(char *name, var_type_e return_type, ast_list_t *params, ast_list_t *stmts) {
+    ast_t *node = (ast_t*)malloc(sizeof(ast_t));
+    if (node) {
+        node->type = AST_FUNCTION;
+        node->function.name = name;
+        node->function.return_type = return_type;
+        node->function.params = params;
+        node->function.stmts = stmts;
+    }
+    return node;
+}
