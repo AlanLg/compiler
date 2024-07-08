6edacf4a32bcdc19ee506d8fbc1083d1ad271668
GregMac <gregorypotesgonzalez@gmail.com>
2024-07-08 14:04:45 +0200

    added ast_new_integer

--- syntax_analysis.c
+++ syntax_analysis.c
@@ -1,3 +1,10 @@
 #include "syntax_analysis.h"
 
-ast_t *ast_new_integer(long val) {}
+ast_t *ast_new_integer(long val) {
+    ast_t *node = (*ast_t) malloc(sizeof(ast_t));
+    if (node) {
+        node->type = AST_INTEGER;
+        node->integer = val;
+    }
+    return node;
+}
