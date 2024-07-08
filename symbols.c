cd9e8ca9b5f8b603f5db2a6ba22423b9e38ec3e1
AlanLg <alanilateur@gmail.com>
2024-07-08 17:10:43 +0200

    added empty methods for symbol.c

---
+++ symbols.c
@@ -0,0 +1,21 @@
+#include "symbols.h"
+
+symbol_t * sym_new (char *name, sym_type_e type, ast_t *attributes) {
+
+}
+
+void sym_delete (symbol_t * sym) {
+
+}
+
+void sym_remove (symbol_t **table, symbol_t *sym) {
+
+}
+
+void sym_add (symbol_t **table, symbol_t *sym) {
+
+}
+
+symbol_t * sym_search (symbol_t *table, char *name) {
+
+}
