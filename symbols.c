d223ca21a3c3f8002ec83d262f108d9f90fc6094
GregMac <gregorypotesgonzalez@gmail.com>
2024-07-09 09:57:33 +0200

    added sym_remove, sym_delete

--- symbols.c
+++ symbols.c
@@ -1,24 +1,74 @@
 #include "symbols.h"
 
 symbol_t * sym_new(char *name, sym_type_e type, ast_t *attributes) {
-  symbol_t *symbol = (symbol_t*)malloc(sizeof(symbol_t));
-  if (symbol) {
+    symbol_t *symbol = (symbol_t*)malloc(sizeof(symbol_t));
+    if (symbol) {
     symbol->name = name;
     symbol->type = type;
     symbol->attributes = attributes;
     symbol->next = NULL;
     symbol->function_table = NULL;
-  }
+    }
 
   return symbol;
 }
 
-void sym_delete(symbol_t * sym) {
+void sym_delete(symbol_t **sym) {
+    symbol_t *current_symbol = *sym;
+    symbol_t *next_symbol;
 
+    while (current_symbol != NULL) {
+        next_symbol = current_symbol->next;
+
+        if (current_symbol->name != NULL) {
+            free(current_symbol->name);
+        }
+        if (current_symbol->attributes != NULL) {
+            free(current_symbol->attributes);
+        }
+        if (current_symbol->function_table != NULL) {
+            sym_delete(&(current_symbol->function_table));
+        }
+
+        free(current_symbol);
+        current_symbol = next_symbol;
+    }
+
+    *sym = NULL;
 }
 
 void sym_remove(symbol_t **table, symbol_t *sym) {
+    if (table == NULL || *table == NULL || sym == NULL) {
+        return;
+    }
 
+    symbol_t *current = *table;
+    symbol_t *previous = NULL;
+
+    if (current == sym) {
+        *table = current->next;
+        free(current->name);
+        free(current->attributes);
+        free(current);
+        return;
+    }
+
+    while (current != NULL && current != sym) {
+        previous = current;
+        current = current->next;
+    }
+
+    if (current == NULL) {
+        return;
+    }
+
+    if (previous != NULL) {
+        previous->next = current->next;
+    }
+
+    free(current->name);
+    free(current->attributes);
+    free(current);
 }
 
 void sym_add(symbol_t **table, symbol_t *sym) {
--- symbols.h
+++ symbols.h
@@ -18,7 +18,7 @@ typedef struct symbol_t {
 } symbol_t;
 
 symbol_t * sym_new (char *name, sym_type_e type, ast_t *attributes);
-void sym_delete (symbol_t * sym);
+void sym_delete (symbol_t ** sym);
 void sym_remove (symbol_t **table, symbol_t *sym);
 void sym_add (symbol_t **table, symbol_t *sym);
 symbol_t * sym_search (symbol_t *table, char *name);
