66785fa0a6f4b5ae57674e38abaf78e92902e13f
GregMac <gregorypotesgonzalez@gmail.com>
2024-07-10 10:38:14 +0200

    fixed type for symbol

--- src/parser.c
+++ src/parser.c
@@ -1,44 +1,52 @@
 #include "../include/parser.h"
 
-symbol_t symbols;
+symbol_t **table;
 
-  /*
-    Début
-     répéter
-     analyse du lexème
-     si le premier lexème n’est pas fonction,
-     arrêter et retourner une erreur
-     sinon
-     appeler analyse_fonction()
-     ajouter l’ast_t* retourné dans la liste des fonctions
-     tant que le fichier n’est pas terminé
-    Fin
-  */
+/*
+  Début
+   répéter
+   analyse du lexème
+   si le premier lexème n’est pas fonction,
+   arrêter et retourner une erreur
+   sinon
+   appeler analyse_fonction()
+   ajouter l’ast_t* retourné dans la liste des fonctions
+   tant que le fichier n’est pas terminé
+  Fin
+*/
 void parse(buffer_t *buffer) {
-  if (!strcmp(lexer_getalphanum(buffer), "fonction")) {
-    perror("first lexer was not \"fonction\"");
-    exit(1);
-  }
+    if (!strcmp(lexer_getalphanum(buffer), "fonction")) {
+        perror("first lexer was not \"fonction\"");
+        exit(1);
+    }
 
-      ast_t *result = analyze_function(buffer);
+    ast_t *result = analyze_function(buffer);
 
 }
 
-  /*
-    Début
-     analyse du lexème qui sera le nom de la fonction
-     appel de analyse_paramètres()
-     appel de analyse_type_de_retour()
-     appel de analyse_corps_de_fonction()
-     création de l’AST pour la fonction “main” avec les paramètres, type de retour et corps de
-    fonction
-     ajout du symbole correspondant au nom de la fonction dans la table des symboles
-     retourner l’AST pour la fonction “main”
-    Fin
-  */
+/*
+  Début
+   analyse du lexème qui sera le nom de la fonction
+   appel de analyse_paramètres()
+   appel de analyse_type_de_retour()
+   appel de analyse_corps_de_fonction()
+   création de l’AST pour la fonction “main” avec les paramètres, type de retour et corps de
+  fonction
+   ajout du symbole correspondant au nom de la fonction dans la table des symboles
+   retourner l’AST pour la fonction “main”
+  Fin
+*/
 ast_t *analyze_function(buffer_t *buffer) {
-  return ast_new_function(lexer_getalphanum(buffer), analyze_return(buffer), analyze_parameters(buffer),analyze_function_body(buffer));
+    char *name = lexer_getalphanum(buffer);
+    ast_list_t *params = analyze_parameters(buffer);
+    var_type_e return_type = analyze_return(buffer);
+    ast_list_t *stmts = analyze_function_body(buffer);
+    ast_t *ast_function = ast_new_function(name, return_type, params, stmts);
+    symbol_t *function_symbol = sym_new(name, SYM_FUNCTION, ast_function);
+    sym_add(table, function_symbol);
+    return ast_function;
 }
+
 /*
   Début
     analyse du prochain symbole de ponctuation
@@ -59,16 +67,16 @@ ast_t *analyze_function(buffer_t *buffer) {
   Fin
 */
 ast_list_t *analyze_parameters(buffer_t *buffer) {
-  if (buf_getchar(buffer) != '(') {
-    perror("missing ( for function parameters");
-    exit(1);
-  }
-  if (lexer_getalphanum(buffer))
-  return ast_list_new_node(ast_new_variable(lexer_getalphanum(buffer), VAR_INTEGER));
+    if (buf_getchar(buffer) != '(') {
+        perror("missing ( for function parameters");
+        exit(1);
+    }
+    if (lexer_getalphanum(buffer))
+        return ast_list_new_node(ast_new_variable(lexer_getalphanum(buffer), VAR_INTEGER));
 }
 
 var_type_e analyze_return(buffer_t *buffer) {
-    if(buf_getchar(buffer) != ':') {
+    if (buf_getchar(buffer) != ':') {
         perror("missing : for function return type");
         exit(1);
     }
