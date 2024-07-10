747a1036265baa32bd80b4e1903ce9c24338687e
AlanLg <alanilateur@gmail.com>
2024-07-10 16:02:19 +0200

    fix issue

---
+++ cmake-build-debug/CMakeFiles/compiler.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/alan/development/projects/c/compiler/src/ast.c" "CMakeFiles/compiler.dir/src/ast.c.o" "gcc" "CMakeFiles/compiler.dir/src/ast.c.o.d"
+  "/home/alan/development/projects/c/compiler/src/buffer.c" "CMakeFiles/compiler.dir/src/buffer.c.o" "gcc" "CMakeFiles/compiler.dir/src/buffer.c.o.d"
+  "/home/alan/development/projects/c/compiler/src/lexer.c" "CMakeFiles/compiler.dir/src/lexer.c.o" "gcc" "CMakeFiles/compiler.dir/src/lexer.c.o.d"
+  "/home/alan/development/projects/c/compiler/src/main.c" "CMakeFiles/compiler.dir/src/main.c.o" "gcc" "CMakeFiles/compiler.dir/src/main.c.o.d"
+  "/home/alan/development/projects/c/compiler/src/parser.c" "CMakeFiles/compiler.dir/src/parser.c.o" "gcc" "CMakeFiles/compiler.dir/src/parser.c.o.d"
+  "/home/alan/development/projects/c/compiler/src/symbols.c" "CMakeFiles/compiler.dir/src/symbols.c.o" "gcc" "CMakeFiles/compiler.dir/src/symbols.c.o.d"
+  "/home/alan/development/projects/c/compiler/src/utils.c" "CMakeFiles/compiler.dir/src/utils.c.o" "gcc" "CMakeFiles/compiler.dir/src/utils.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
