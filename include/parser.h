3fd1e8c4ccc49f1a2574f220d0c8b813137529dc
GregMac <gregorypotesgonzalez@gmail.com>
2024-07-14 20:05:28 +0200

    reorganized functions

--- include/parser.h
+++ include/parser.h
@@ -8,18 +8,14 @@
 #include <stdlib.h>
 
 void parse(buffer_t *buffer);
-
 symbol_t *analyze_function(buffer_t *buffer);
 symbol_t *analyze_parameters(buffer_t *buffer);
-var_type_e analyze_return(buffer_t *buffer);
 symbol_t *analyze_function_body(buffer_t *buffer);
-
 symbol_t *analyze_conditional_branching(buffer_t *buffer);
-
 symbol_t *analyze_loop(buffer_t *buffer);
-
 symbol_t *analyze_assignment(buffer_t *buffer);
-
 symbol_t *analyze_declaration(buffer_t *buffer);
+
+var_type_e analyze_return(buffer_t *buffer);
 
 #endif
